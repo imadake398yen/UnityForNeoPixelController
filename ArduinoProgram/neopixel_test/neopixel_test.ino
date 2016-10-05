530511a9689b82567f67e72a8cd3780e42694a58
falcon <imadake398yen@gmail.com>
2016-10-05 12:18:27 +0900

    initial commit

---
+++ ArduinoProgram/neopixel_test/neopixel_test.ino
@@ -0,0 +1,71 @@
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+
+#define PIN1        6
+#define PIN2        7
+#define NUMPIXELS   50
+
+class RGB {
+  public: bool reach = true;
+  private:int lerpSpeed = 10;
+  public: int r, g, b, tr, tg, tb;
+  public: void SetColor (int red, int green, int blue){
+    r = red; g = green; b = blue;
+  }
+  public: void SetTargetColor (int red, int green, int blue){
+    r = red; g = green; b = blue;
+  }
+  public: void TargetLerp () {
+    int diffr = tr - r;
+    int diffg = tg - g;
+    int diffb = tb - b;
+    int vr = (diffr / lerpSpeed); 
+    int vg = (diffg / lerpSpeed); 
+    int vb= (diffb / lerpSpeed);
+    r += vr;
+    g += vg;
+    b += vb;
+    if (abs(diffr) < lerpSpeed*2 && abs(diffg) < lerpSpeed*2 && abs(diffb) < lerpSpeed*2) {
+      reach = true;
+    } else reach = false;
+  }
+  
+};
+RGB color;
+
+Adafruit_NeoPixel pixels1 = Adafruit_NeoPixel(NUMPIXELS, PIN1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels2 = Adafruit_NeoPixel(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
+int delayVal = 20; 
+
+void setup() {
+  pixels1.begin();
+  pixels2.begin();
+}
+
+void loop() {
+  if (color.reach) {
+    color.reach = false;
+    color.SetTargetColor (random(0, 255), random(0, 255), random(0, 255));
+    for(int i=0;i <NUMPIXELS; i++){
+      updateLED (i, color); 
+      delay(delayVal*2);
+    }
+  }
+  else {
+    color.TargetLerp();
+    for(int i=0;i <NUMPIXELS; i++){
+      updateLED (i, color); 
+    }
+  } 
+  delay(delayVal);
+}
+
+void updateLED (int num, RGB c) {
+  pixels1.setPixelColor(num, pixels1.Color(c.r, c.g, c.b));
+  pixels1.show(); 
+  pixels2.setPixelColor(num, pixels2.Color(c.r, c.g, c.b));
+  pixels2.show(); 
+}
+
+
+
---
+++ LED_UNITY_SYSTEM/Assets/LED_Assets/Scripts/LEDManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using LitJson;
+
+
+[System.SerializableAttribute]
+public struct MatrixSettings { public int col, row; }
+
+public class LEDString { public RGB[] leds; }
+public class RGB {
+	const int coefficient = 255;
+	public int r, g, b;
+	public void SetRgbByColor (Color color) {
+		r = (int)(color.r * coefficient);
+		g = (int)(color.g * coefficient);
+		b = (int)(color.b * coefficient);
+	}
+}
+
+
+public class LEDManager 
+: SingletonMonoBehaviour<LEDManager> {
+
+	public LEDString[] ledStrings;
+	public MatrixSettings settings;
+	public GameObject ledPrefab;
+
+
+	private void Start () {
+		
+		List<LEDString> strs = new List<LEDString>();
+		for (int i=0; i<settings.col; i++) {
+			strs.Add( new LEDString() );
+			
+			List<RGB> leds = new List<RGB>();
+			for (int j=0; j<settings.row; j++) {
+				leds.Add( new RGB() );
+			}
+			strs[i].leds = leds.ToArray();
+		}
+		ledStrings = strs.ToArray();
+
+		for (int i=0; i<ledStrings.Length; i++) {
+			for (int j=0; j<ledStrings[i].leds.Length; j++) {
+
+				Vector3 spawnPos = new Vector3 (i,0,j);
+
+				GameObject obj = (GameObject)Instantiate(
+					ledPrefab, spawnPos, Quaternion.identity);
+
+				obj.GetComponent<LEDController>().rgb 
+					= ledStrings[i].leds[j];
+			}
+		}
+
+	}
+
+
+	private void Update () {
+		if (Input.GetKeyDown("space")) {
+			string[] json = GetLEDStingJson();
+			print(json[0]);
+		}
+	}
+
+
+	public string[] GetLEDStingJson () {
+		string[] jsons = new string[ledStrings.Length]; 
+		for (int i=0; i<ledStrings.Length; i++) {
+			jsons[i] = JsonMapper.ToJson( ledStrings[i] );
+		} 
+		return jsons;
+	}
+}
+
+
+
