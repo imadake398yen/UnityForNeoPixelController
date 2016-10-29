a11f32a62d5051d2761e4558b783dea959d29bc8
falcon <imadake398yen@gmail.com>
2016-10-30 03:23:59 +0900

    これで動くといいな、と願いを込めた

--- ArduinoProgram/neopixel_test/neopixel_test.ino
+++ ArduinoProgram/neopixel_test/neopixel_test.ino
@@ -5,12 +5,14 @@
 #define NUMPIXELS  50
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN1, NEO_GRB + NEO_KHZ800);
-int delayVal = 20; 
-int current = 0;
+
+int moveDelay = 20;     //LEDの色の流れる速度
+int pulseDelay = 100;   //LEDが流れる周期
+int colorSpeed = 10;    //LED1個単位の黒に変わる速度
+int current = 0;        //名前迷った。ごめん。
 
 class LED {
   public: int ledNum;
-  private:int lerpSpeed = 10;
   private:int darkColor = 0;
   public: int r, g, b;
   public: void SetColor (int red, int green, int blue){
@@ -21,44 +23,40 @@ class LED {
     int diffr = darkColor - r;
     int diffg = darkColor - g;
     int diffb = darkColor - b;
-    int vr = (diffr / lerpSpeed); 
-    int vg = (diffg / lerpSpeed); 
-    int vb = (diffb / lerpSpeed);
+    int vr = (diffr / colorSpeed); 
+    int vg = (diffg / colorSpeed); 
+    int vb = (diffb / colorSpeed);
     r += vr;
     g += vg;
     b += vb;
     updateLED();
   }
-
   public: void updateLED () {
-    pixels.setPixelColor(ledNum, pixels.Color(r,g,b));
+    pixels.setPixelColor( ledNum, pixels.Color(r,g,b) );
     pixels.show(); 
   }
 };
 LED led[NUMPIXELS];
 
-
 void setup() { 
   pixels.begin(); 
   for (int i=0; i<NUMPIXELS; i++) {
-    led[i].ToDark();
+    led[i].ledNum = i;
   }
 }
 
 void loop() {
-  for (int i=0; i<NUMPIXELS; i++) {
-    led[i].ToDark();
-  }
-  
-  led[current].SetColor( 255, 0, 150 );
-  
-  if (current < NUMPIXELS-1) {
+  if ( current < NUMPIXELS-1 ) {
+    for (int i=0; i<NUMPIXELS; i++) {
+      led[i].ToDark();
+    }
+    led[current].SetColor( 255, 0, 150 );
+    delay( moveDelay );
     current += 1;
   } else {
+    delay( pulseDelay );
     current = 0;
   }
-  
-  delay(delayVal);
 }
 
 
